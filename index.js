8c1692a20d0fc38fefc999317ecf22c44eaf37fd
Grace Tan <gracetan37@gmail.com>
2019-01-28 07:44:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyRemoveLastKitten()
